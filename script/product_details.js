43c7459502aa7b12ca08de10d60e3b47501b67ec
igorgll <igorlimagn@gmail.com>
2023-06-10 20:40:19 -0300

    @igorgll [Adicionando método de carregar dinâmicamente as imagens dos produtos]

--- script/product_details.js
+++ script/product_details.js
@@ -1,19 +1,3 @@
-let ProductImg = document.getElementById("ProductImg");
-let SmallImg = document.getElementsByClassName("small-img");
-
-SmallImg[0].onclick = function () {
-  ProductImg.src = SmallImg[0].src;
-};
-SmallImg[1].onclick = function () {
-  ProductImg.src = SmallImg[1].src;
-};
-SmallImg[2].onclick = function () {
-  ProductImg.src = SmallImg[2].src;
-};
-SmallImg[3].onclick = function () {
-  ProductImg.src = SmallImg[3].src;
-};
-
 const productTexts = [
   {
     id: "1",
@@ -22,6 +6,10 @@ const productTexts = [
     productDetails:
       "Dê um upgrade de estilo ao seu guarda-roupa de verão com a Camiseta Ativa Masculina HRX. Combine-a com um shorts para o treino matinal ou com uma calça jeans para sair com os amigos à noite.",
     productImgSrc: "images/product-1.jpg",
+    productImgSrcSmall_1: "images/gallery-1.jpg",
+    productImgSrcSmall_2: "images/gallery-2.jpg",
+    productImgSrcSmall_3: "images/gallery-3.jpg",
+    productImgSrcSmall_4: "images/gallery-4.jpg",
   },
   {
     id: "2",
@@ -30,6 +18,10 @@ const productTexts = [
     productDetails:
       "O tênis de corrida preto HRX oferece estilo, conforto e desempenho para corredores. Com um design moderno e durável, ele possui amortecimento responsivo e tração confiável em diferentes superfícies. O ajuste confortável e seguro proporciona suporte durante a corrida, ajudando você a alcançar seus objetivos.",
     productImgSrc: "images/product-2.jpg",
+    productImgSrcSmall_1: "images/small-img-2__1.jpg",
+    productImgSrcSmall_2: "images/small-img-2__2.jpg",
+    productImgSrcSmall_3: "images/small-img-2__3.jpg",
+    productImgSrcSmall_4: "images/small-img-2__4.jpg",
   },
   {
     id: "3",
@@ -37,7 +29,7 @@ const productTexts = [
     productPrice: "R$200.00",
     productDetails:
       "A calça cinza HRX é uma peça versátil e estilosa para o seu guarda-roupa. Feita com materiais de qualidade, essa calça proporciona conforto e durabilidade. Seu design moderno e elegante combina perfeitamente com diversas ocasiões, permitindo que você crie looks casuais ou mais sofisticados.",
-      productImgSrc: "images/product-3.jpg"
+    productImgSrc: "images/product-3.jpg",
   },
   {
     id: "4",
@@ -45,7 +37,7 @@ const productTexts = [
     productPrice: "R$200.00",
     productDetails:
       "Confortável e versátil, a camisa polo Essentials Pique traz modelagem regular fit, punhos canelados e algodão proveniente da parceria PUMA x Iniciativa Better Cotton.",
-      productImgSrc: "images/product-4.jpg"
+    productImgSrc: "images/product-4.jpg",
   },
   {
     id: "5",
@@ -53,7 +45,7 @@ const productTexts = [
     productPrice: "R$500.00",
     productDetails:
       "O tênis cinza Breezy é uma escolha perfeita para os amantes de calçados confortáveis e estilosos. Com um design moderno e contemporâneo, ele combina perfeitamente com diversos looks casuais.",
-      productImgSrc: "images/product-5.jpg"
+    productImgSrc: "images/product-5.jpg",
   },
   {
     id: "6",
@@ -61,7 +53,7 @@ const productTexts = [
     productPrice: "R$170.00",
     productDetails:
       "Caminhe com orgulho e estilo com esta camiseta Graphic Block, estampada com a marca PUMA em um design “color block“. Declaradamente casual, esta é a maneira perfeita de trazer energia em dias discretos.",
-      productImgSrc: "images/product-6.jpg"
+    productImgSrc: "images/product-6.jpg",
   },
   {
     id: "7",
@@ -69,7 +61,7 @@ const productTexts = [
     productPrice: "R$60.00",
     productDetails:
       "A meia HRX é o complemento perfeito para garantir conforto e desempenho durante suas atividades físicas. Com um design ergonômico e tecnologia avançada, ela proporciona um ajuste preciso e confortável aos seus pés.",
-      productImgSrc: "images/product-7.jpg"
+    productImgSrc: "images/product-7.jpg",
   },
   {
     id: "8",
@@ -77,7 +69,7 @@ const productTexts = [
     productPrice: "R$1750.00",
     productDetails:
       "O relógio Fossi é uma peça de alta qualidade e estilo atemporal. Com um design sofisticado e detalhes cuidadosamente trabalhados, ele é uma escolha elegante para homens e mulheres que apreciam acessórios refinados.",
-      productImgSrc: "images/product-8.jpg"
+    productImgSrc: "images/product-8.jpg",
   },
   {
     id: "9",
@@ -85,7 +77,7 @@ const productTexts = [
     productPrice: "R$2500.00",
     productDetails:
       "O relógio Roadmaster é uma verdadeira obra-prima da relojoaria. Combinando design excepcional e artesanato de alta qualidade, esse relógio é uma expressão de elegância e sofisticação.",
-      productImgSrc: "images/product-9.jpg"
+    productImgSrc: "images/product-9.jpg",
   },
   {
     id: "10",
@@ -93,7 +85,7 @@ const productTexts = [
     productPrice: "R$90.00",
     productDetails:
       "O tênis de corrida HRX é o companheiro perfeito para os entusiastas da corrida que buscam desempenho, conforto e estilo. Projetado com tecnologia avançada, ele oferece suporte e amortecimento excepcionais para ajudar você a alcançar seus melhores resultados.",
-      productImgSrc: "images/product-10.jpg"
+    productImgSrc: "images/product-10.jpg",
   },
   {
     id: "11",
@@ -101,7 +93,7 @@ const productTexts = [
     productPrice: "R$185.00",
     productDetails:
       "O tênis Petite Jolie é uma combinação perfeita de estilo e conforto. Com um design moderno e feminino, ele é ideal para mulheres que desejam estar na moda e não abrem mão do conforto em seu dia a dia.",
-      productImgSrc: "images/product-11.jpg"
+    productImgSrc: "images/product-11.jpg",
   },
   {
     id: "12",
@@ -109,7 +101,7 @@ const productTexts = [
     productPrice: "R$250.00",
     productDetails:
       "A calça jogger Nike é a escolha perfeita para quem busca estilo e conforto em um único produto. Com um design moderno e atlético, ela combina perfeitamente com um estilo de vida ativo e descontraído.",
-      productImgSrc: "images/product-12.jpg"
+    productImgSrc: "images/product-12.jpg",
   },
 ];
 
@@ -134,6 +126,19 @@ function showProductDetails(productId) {
 
     let productImg = document.getElementById("productImg");
     productImg.src = product.productImgSrc;
+
+    let smallImg = document.getElementsByClassName("small-img");
+    smallImg[0].src = product.productImgSrcSmall_1;
+    smallImg[1].src = product.productImgSrcSmall_2;
+    smallImg[2].src = product.productImgSrcSmall_3;
+    smallImg[3].src = product.productImgSrcSmall_4;
+
+    // LOOP PARA CADA MINIATURA PARA MUDAR IMG PRINCIPAL
+    for (let i = 0; i < smallImg.length; i++) {
+      smallImg[i].addEventListener("click", function () {
+        productImg.src = smallImg[i].src;
+      });
+    }
   }
 }
 
